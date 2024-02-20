7e8bcf6b57f1bc24cc98ab8e64834176d1f6a063
KlaraLindner <klara.lindner@stud.htwk-leipzig.de>
2024-02-20 11:47:08 +0100

    Updated Trees and WeatherData
    -Spruce Tree Lcoation Changed
    - Spruce made to Preafb
    - Added CSV to Weather n ClimateHandler

--- Simlation-poly/Assets/World/Environment/Spawn/TreeSpawner.cs
+++ Simlation-poly/Assets/World/Environment/Spawn/TreeSpawner.cs
@@ -9,19 +9,19 @@ namespace World.Environment.Spawn
     public class TreeSpawner : Spawner
     {
         public int deceaseCounter = 0;
-        
+
         public TreeSpawner()
         {
             spawnAttempts = 550;
             minHeight = 3.5f;
             maxHeight = 7.5f;
         }
-
+        //TODO - Change the value to real world related data like size, age, ground structure usw.
         public override void SpawnOptions(GameObject newPrefab, RaycastHit hit)
         {
             var plant = Instantiate(newPrefab, hit.point, new Quaternion(0f, Random.Range(0f, 360f), 0f, 0f), transform);
             var tree = plant.GetComponent<TreeAgent>();
-            
+
             tree.co2Modifier = Random.Range(tree.co2Modifier * 0.75f, tree.co2Modifier * 1.25f);
             tree.o2Modifier = Random.Range(tree.o2Modifier * 0.75f, tree.o2Modifier * 1.25f);
             tree.waterConsumption = Random.Range(tree.waterConsumption * 0.75f, tree.waterConsumption * 1.25f);
@@ -30,13 +30,13 @@ namespace World.Environment.Spawn
             {
                 tree.AddDisease(new BarkBeetle());
             }
-            else if(Random.Range(0f, 1f) > 0.8f)
+            else if (Random.Range(0f, 1f) > 0.8f)
             {
                 tree.AddDisease(new BarkBeetle());
             }
-            
+
             deceaseCounter++;
-            
+
             RegisterFloraAgent(tree);
 
             var scale = Random.Range(0.4f, 1);
